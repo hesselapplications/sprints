cc6365f842d43cfc6dc32411c2dc63ed623da0f8
hesselapplications <hesselapplications@gmail.com>
2020-03-18 22:33:12 -0500

    Add pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  "transpileDependencies": [
+  transpileDependencies: [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: "BackTask",
+    themeColor: "#7E57C2",
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
 }
