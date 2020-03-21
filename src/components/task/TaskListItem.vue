ef2a5c6143d4dc4fb45c4da1d0ab4fa2764e0f03
hesselapplications <hesselapplications@gmail.com>
2020-03-20 21:30:55 -0500

    Use subdirectory arrow

--- src/components/task/TaskListItem.vue
+++ src/components/task/TaskListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <v-list-item @click="goToTask">
     <v-list-item-action>
-      <v-icon v-if="hasChildren">mdi-menu-right</v-icon>
+      <v-icon v-if="hasChildren" color="grey lighten-1">mdi-subdirectory-arrow-right</v-icon>
       <v-checkbox
         v-else
         @click.stop="toggleComplete(task.id)"
