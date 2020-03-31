e9fb9c9cb2760f7fcd2e87213772ca7e33621ca7
hesselapplications <hesselapplications@gmail.com>
2020-03-30 23:20:36 -0500

    Add search functionality

---
+++ src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-text-field
+    v-model="model"
+    label="Search"
+    hide-details
+    solo
+    prepend-inner-icon="mdi-magnify"
+    background-color="grey lighten-3"
+    flat
+  ></v-text-field>
+</template>
+
+<script>
+import vmodel from "@/mixins/vmodel.js";
+
+export default {
+  mixins: [vmodel]
+};
+</script>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,12 +1,47 @@
 <template>
   <v-container>
-    <v-row align="center" justify="center">
-      <v-col class="text-center">
-        <v-icon color="grey lighten-1">mdi-magnify</v-icon>
+    <v-row justify="center">
+      <v-col xl="6" lg="8" md="10" cols="12">
+        <v-card>
+          <!-- SEARCH -->
+          <search class="pa-4" v-model="search"></search>
+
+          <v-divider></v-divider>
+
+          <!-- RESULTS -->
+          <v-treeview ref="results" class="py-4" :search="search" :items="taskTrees" open-on-click>
+            <template v-slot:append="{ item }">
+              <v-btn icon :to="`/tasks/${item.id}`" color="grey lighten-1">
+                <v-icon>mdi-menu-right</v-icon>
+              </v-btn>
+            </template>
+          </v-treeview>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
-export default {};
+import Search from "@/components/Search";
+import { mapGetters } from "vuex";
+
+export default {
+  components: {
+    Search
+  },
+  data() {
+    return {
+      search: null
+    };
+  },
+  watch: {
+    search(search) {
+      var searching = search.length > 0;
+      this.$refs.results.updateAll(searching);
+    }
+  },
+  computed: {
+    ...mapGetters(["taskTrees"])
+  }
+};
 </script>
